5983a6ef278f3d73775f622afbe9bf26f2abe198
Aiaiioi <47210717+Aiaiioi@users.noreply.github.com>
2023-02-03 05:04:02 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/Aiaiioi',],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'Aiaiioi@maoioi.onmicrosoft.com',
@@ -53,11 +53,7 @@ module.exports = {
   links: [
     {
       name: '笔记',
-      link: 'https://aiaiioi.gitee.io/',
-    },
-    {
-      name: '笔记',
-      link: 'https://aiaiioi.pages.dev/',
+      link: 'https://notes.397900.xyz/',
     },
   ],
 
